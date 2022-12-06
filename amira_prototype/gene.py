2d50898240ed2f3c6eeddd58167756e519bf2f01
Danderson123 <danielanderson1@hotmail.com>
2022-12-06 12:33:34 +0000

    improve annotated of gene.py code

--- amira_prototype/gene.py
+++ amira_prototype/gene.py
@@ -1,5 +1,8 @@
 def convert_string_strand_to_int(stringStrand: str) -> int:
+    """ convert a string strand to an integer strand and return the value """
+    # ensure the string strand is "+" or "-" only
     assert stringStrand == "+" or stringStrand == "-"
+    # convert the string strand to the necessary integer
     if stringStrand == "+":
         intStrand = +1
     else:
@@ -7,23 +10,37 @@ def convert_string_strand_to_int(stringStrand: str) -> int:
     return intStrand
 
 def split_gene_and_strand(gene: str):
+    """ returns the separated string name and integer strand for an input gene with strand information """
+    # ensure the input gene is not an empty string
     assert gene.replace(" ", "") != "", "Gene information is missing"
+    # isolate the strand
     geneStringStrand = gene[0]
-    geneName = gene[1:]
+    # isolate the gene name and replace any spaces with an underscore
+    geneName = gene[1:].replace(" ", "_")
+    # ensure the strand is only either "+" or "-"
     assert geneStringStrand == "-" or geneStringStrand == "+", "Strand information missing for: " + gene
+    # ensure that the name of the gene is not missing
     assert geneName != "", "Gene name information missing for: " + gene
+    # convert the string strand to an integer, either 1 or -1
     geneStrand = convert_string_strand_to_int(geneStringStrand)
     return geneName, geneStrand
 
 def reverse_strand(geneStrand: int) -> int:
+    """ returns the reverse sign for an input integer strand """
+    # ensure the integer strand input is only 1 or -1
     assert geneStrand == -1 or geneStrand == 1
+    # reverse the input strand by multiplying by -1
     reverseStrandInt = geneStrand * -1
     return reverseStrandInt
 
 def convert_int_strand_to_string(intStrand: int) -> str:
+    """ returns a string strand consistent with the integer strand inputted """
+    # ensure the integer strand input is only 1 or -1
     assert intStrand == -1 or intStrand == 1
+    # if the integer strand is -1 return "-"
     if intStrand == -1:
         stringStrand = "-"
+    # if the integer strand is +1 return "+"
     else:
         stringStrand = "+"
     return stringStrand
@@ -33,10 +50,13 @@ class Gene:
                 gene: str):
         self.name, self.strand = split_gene_and_strand(gene)
     def get_name(self) -> str:
+        """ return the name of the gene without strand information """
         return self.name
     def get_strand(self) -> str:
+        """ return the integer strand of the gene without gene name information"""
         return self.strand
     def reverse_gene(self):
+        """ return a Gene object that is the reverse of this Gene object """
         reverseStrandInt = reverse_strand(self.strand)
         reverseStrand = convert_int_strand_to_string(reverseStrandInt)
         reverseGene = reverseStrand + self.name
@@ -44,6 +64,8 @@ class Gene:
         return GeneData
     def __eq__(self,
             otherGene) -> bool:
+        """ return a bool to check if the gene names of two Gene objects are the same """
         return self.name == otherGene.get_name()
     def __hash__(self):
+        """ return a hash of the gene name to compare against hashes of other Gene instances """
         return hash(self.name)
