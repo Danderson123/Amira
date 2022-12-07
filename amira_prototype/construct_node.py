b4b00e5fb1d85fb990c267cc0d099c2d4279ac83
Danderson123 <danielanderson1@hotmail.com>
2022-12-07 10:59:40 +0000

    add Node constructor and unit tests

---
+++ amira_prototype/construct_node.py
@@ -0,0 +1,44 @@
+from construct_read import Read
+from construct_read import GeneMer
+
+class Node:
+    def __init__(self,
+                GeneMer: GeneMer,
+                nodeId: int):
+        self.GeneMer = GeneMer
+        self.nodeCoverage = 0
+        self.setOfReads = set()
+        self.nodeId = nodeId
+    def get_GeneMer(self):
+        """ return the GeneMer object represented by this node """
+        return self.GeneMer
+    def get_nodeId(self):
+        """ return a unique integer node ID for this node """
+        return self.nodeId
+    def get_coverage(self) -> int:
+        """ return the number of time we have seen this geneMer """
+        return self.nodeCoverage
+    def increment_coverage(self) -> int:
+        """ increase the coverage of this node by 1 and return the new coverage """
+        self.nodeCoverage += 1
+        return self.nodeCoverage
+    def get_reads(self) -> list:
+        """ return a generator for the list of Read objects associated with this node """
+        for read in self.setOfReads:
+            yield read
+    def add_read(self,
+                read: Read):
+        """ add a read object to a list of reads for this node """
+        self.setOfReads.add(read)
+    def remove_read(self,
+                    read: Read):
+        """ remove a read from the list of reads for this node """
+        if read in self.setOfReads:
+            readList = list(self.setOfReads)
+            mask = readList.index(read)
+            del readList[mask]
+            self.setOfReads = readList
+    def __eq__(self,
+            otherNode):
+        """ check if two nodes are identical """
+        return self == otherNode
---
+++ test/test_construct_node.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.insert(0, "../amira_prototype")
+
+from construct_node import Node
+from construct_read import Read
+
+def test_node_increment_coverage():
+    genes = ["+gene1", "-gene2", "+gene3", "-gene4", "+gene5", "-gene6"]
+    read = Read("read1",
+                genes)
+    geneMers = [x for x in read.get_geneMers(3)]
+    for g in geneMers:
+        node = Node(g, 1)
+        for i in range(5):
+            coverage = node.increment_coverage()
+            assert coverage == i + 1
+
+def test_add_read():
+    genes = ["+gene1", "-gene2", "+gene3", "-gene4", "+gene5", "-gene6"]
+    read1 = Read("read1",
+                genes)
+    read2 = Read("read2",
+                genes)
+    read3 = Read("read3",
+                genes)
+    geneMers = [x for x in read1.get_geneMers(3)]
+    for g in geneMers:
+        node = Node(g, 1)
+        readList = [read1, read1, read2, read3, read3]
+        for read in readList:
+            node.add_read(read)
+        assert all(r in node.get_reads() for r in readList), "A read is missing from the node"
+        assert all([x for x in node.get_reads()].count(r) == 1 for r in readList), "A read occurs more than once in the read list"
+
+def test_remove_read():
+    genes = ["+gene1", "-gene2", "+gene3", "-gene4", "+gene5", "-gene6"]
+    read1 = Read("read1",
+                genes)
+    read2 = Read("read2",
+                genes)
+    read3 = Read("read3",
+                genes)
+    geneMers = [x for x in read1.get_geneMers(3)]
+    # test removing a read that is in the list
+    for g in geneMers:
+        node = Node(g, 1)
+        readList = [read1, read1, read2, read3, read3]
+        for read in readList:
+            node.add_read(read)
+        for read in readList:
+            node.remove_read(read)
+            assert not read in node.get_reads()
+    # test removing a read that is not in the list
+    read4 = Read("read4",
+                genes)
+    for g in geneMers:
+        node = Node(g, 1)
+        readList = [read1, read1, read2, read3, read3]
+        for read in readList:
+            node.add_read(read)
+    node.remove_read(read4)
+    assert all(r in node.get_reads() for r in readList), "A read was incorrectly removed from the Node"
+    assert all([x for x in node.get_reads()].count(r) == 1 for r in readList), "A read occurs more than once in the read list"
+
+
+sys.stderr.write("Testing construct_node: Node.increment_coverage\n")
+test_node_increment_coverage()
+sys.stderr.write("Test passed\n")
+sys.stderr.write("Testing construct_node: Node.add_read\n")
+test_add_read()
+sys.stderr.write("Test passed\n")
+sys.stderr.write("Testing construct_node: Node.remove_read\n")
+test_remove_read()
+sys.stderr.write("Test passed\n")
